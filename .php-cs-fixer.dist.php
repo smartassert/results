ddbd83a6e1323458e6306f87c9bc61ffe6810cd0
Jon Cram <webignition@gmail.com>
2022-05-17 11:34:23 +0100

    Create project skeleton (#2)

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/bin')
+    ->in(__DIR__ . '/public')
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        '@PhpCsFixer' => true,
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'php_unit_internal_class' => false,
+        'php_unit_test_class_requires_covers' => false,
+    ])
+    ->setFinder($finder)
+    ;
