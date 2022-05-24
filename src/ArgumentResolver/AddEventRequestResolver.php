2b3859463098ff1cf494463f8b625a296a722425
Jon Cram <webignition@gmail.com>
2022-05-24 10:03:05 +0100

    Create request model resolver

---
+++ src/ArgumentResolver/AddEventRequestResolver.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ArgumentResolver;
+
+use App\Request\AddEventRequest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+class AddEventRequestResolver implements ArgumentValueResolverInterface
+{
+    public function supports(Request $request, ArgumentMetadata $argument): bool
+    {
+        return 'POST' === $request->getMethod() && AddEventRequest::class === $argument->getType();
+    }
+
+    /**
+     * @return \Traversable<AddEventRequest>
+     */
+    public function resolve(Request $request, ArgumentMetadata $argument): \Traversable
+    {
+        if ($this->supports($request, $argument)) {
+            $label = $request->request->get(AddEventRequest::KEY_JOB_LABEL);
+            $label = is_string($label) ? trim($label) : null;
+
+            $type = $request->request->get(AddEventRequest::KEY_TYPE);
+            $type = is_string($type) ? trim($type) : null;
+
+            $reference = $request->request->get(AddEventRequest::KEY_REFERENCE);
+            $reference = is_string($reference) ? trim($reference) : null;
+
+            $payload = null;
+            $payloadContent = $request->request->get(AddEventRequest::KEY_PAYLOAD);
+            if (is_string($payloadContent)) {
+                $payload = json_decode($payloadContent, true);
+                $payload = is_array($payload) ? $payload : null;
+            }
+
+            yield new AddEventRequest($label, $type, $reference, $payload);
+        }
+    }
+}
