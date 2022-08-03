14ef872ac2680242e2dc4d7b6c53bb41cd383973
Jon Cram <webignition@gmail.com>
2022-08-03 17:37:17 +0100

    Create entity for event reference

---
+++ src/Repository/ReferenceRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Reference;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Reference>
+ *
+ * @method null|Reference find($id, $lockMode = null, $lockVersion = null)
+ * @method null|Reference findOneBy(array $criteria, array $orderBy = null)
+ * @method Reference[]    findAll()
+ * @method Reference[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ReferenceRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Reference::class);
+    }
+
+    public function add(Reference $entity): void
+    {
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+    }
+}
