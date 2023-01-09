80e233224f7257d5cfd5083ec1f90cdb506eae3e
Jon Cram <webignition@gmail.com>
2023-01-09 16:34:29 +0000

    Factor out application job-related request method tests (part 2)

---
+++ tests/Integration/JobTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration;
+
+use App\Tests\Application\AbstractJobTest;
+
+class JobTest extends AbstractJobTest
+{
+    use GetClientAdapterTrait;
+}
