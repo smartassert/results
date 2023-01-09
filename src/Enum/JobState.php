72b31eee86c3fb2932fe544014e8754502efbc8f
Jon Cram <webignition@gmail.com>
2023-01-09 17:33:21 +0000

    Fix enum blank lines

--- src/Enum/JobState.php
+++ src/Enum/JobState.php
@@ -7,16 +7,10 @@ namespace App\Enum;
 enum JobState: string
 {
     case STARTED = 'started';
-
     case COMPILING = 'compiling';
-
     case COMPILED = 'compiled';
-
     case EXECUTING = 'executing';
-
     case EXECUTED = 'executed';
-
     case ENDED = 'ended';
-
     case UNKNOWN = 'unknown';
 }
