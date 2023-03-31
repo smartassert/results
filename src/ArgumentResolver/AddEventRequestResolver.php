62ee66659ea96a849231f687669cd39cad90704a
Jon Cram <webignition@gmail.com>
2023-03-31 11:27:58 +0100

    Use Symfony 6.2 (#147)

--- src/ArgumentResolver/AddEventRequestResolver.php
+++ src/ArgumentResolver/AddEventRequestResolver.php
@@ -7,34 +7,32 @@ namespace App\ArgumentResolver;
 use App\Request\AddEventRequest;
 use App\Request\AddEventRequestFactory;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
-class AddEventRequestResolver implements ArgumentValueResolverInterface
+class AddEventRequestResolver implements ValueResolverInterface
 {
     public function __construct(
         private readonly AddEventRequestFactory $addEventRequestFactory,
     ) {
     }
 
-    public function supports(Request $request, ArgumentMetadata $argument): bool
-    {
-        return
-            'POST' === $request->getMethod()
-            && 'application/json' === $request->headers->get('content-type')
-            && AddEventRequest::class === $argument->getType();
-    }
-
     /**
-     * @return \Traversable<AddEventRequest>
+     * @return AddEventRequest[]
      */
-    public function resolve(Request $request, ArgumentMetadata $argument): \Traversable
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
-        if ($this->supports($request, $argument)) {
-            $requestData = json_decode($request->getContent(), true);
-            $requestData = is_array($requestData) ? $requestData : [];
-
-            yield $this->addEventRequestFactory->create($requestData);
+        if (
+            'POST' !== $request->getMethod()
+            || 'application/json' !== $request->headers->get('content-type')
+            || AddEventRequest::class !== $argument->getType()
+        ) {
+            return [];
         }
+
+        $requestData = json_decode($request->getContent(), true);
+        $requestData = is_array($requestData) ? $requestData : [];
+
+        return [$this->addEventRequestFactory->create($requestData)];
     }
 }
