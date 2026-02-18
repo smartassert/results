a8041700be66762b08ef56baf854486e1a92f910
Jon Cram <webignition@gmail.com>
2026-02-18 14:52:06 +0000

    Factor out add event bad requests tests (#250)

---
+++ tests/Functional/Application/AddEventBadRequestTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Application;
+
+use App\Tests\Application\AbstractAddEventBadRequestTest;
+
+class AddEventBadRequestTest extends AbstractAddEventBadRequestTest
+{
+    use GetClientAdapterTrait;
+}
