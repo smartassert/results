75dd4db4867a45ef112773f76e3f98e5ced6bc4f
Jon Cram <webignition@gmail.com>
2026-02-18 14:35:09 +0000

    Factor out event-related bad request method tests (#248)

--- tests/Application/AbstractBadRequestMethodTest.php
+++ tests/Application/AbstractBadRequestMethodTest.php
@@ -9,8 +9,8 @@ use Symfony\Component\Uid\Ulid;
 
 abstract class AbstractBadRequestMethodTest extends AbstractApplicationTest
 {
-    #[DataProvider('createBadMethodDataProvider')]
-    public function testRequestBadMethod(string $method): void
+    #[DataProvider('createJobBadMethodDataProvider')]
+    public function testCreateJobBadMethod(string $method): void
     {
         $label = (string) new Ulid();
 
@@ -26,9 +26,67 @@ abstract class AbstractBadRequestMethodTest extends AbstractApplicationTest
     /**
      * @return array<mixed>
      */
-    public static function createBadMethodDataProvider(): array
+    public static function createJobBadMethodDataProvider(): array
     {
         return [
+            'PUT' => [
+                'method' => 'PUT',
+            ],
+            'DELETE' => [
+                'method' => 'DELETE',
+            ],
+        ];
+    }
+
+    #[DataProvider('addEventBadMethodDataProvider')]
+    public function testAddEventBadMethod(string $method): void
+    {
+        $response = $this->applicationClient->makeEventAddRequest((string) new Ulid(), [], $method);
+
+        self::assertSame(405, $response->getStatusCode());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public static function addEventBadMethodDataProvider(): array
+    {
+        return [
+            'GET' => [
+                'method' => 'GET',
+            ],
+            'PUT' => [
+                'method' => 'PUT',
+            ],
+            'DELETE' => [
+                'method' => 'DELETE',
+            ],
+        ];
+    }
+
+    #[DataProvider('listEventsBadMethodDataProvider')]
+    public function testListEventsBadMethod(string $method): void
+    {
+        $response = $this->applicationClient->makeEventListRequest(
+            (string) new Ulid(),
+            (string) new Ulid(),
+            md5((string) rand()),
+            null,
+            $method
+        );
+
+        self::assertSame(405, $response->getStatusCode());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public static function listEventsBadMethodDataProvider(): array
+    {
+        return [
+            'POST' => [
+                'method' => 'POST',
+            ],
             'PUT' => [
                 'method' => 'PUT',
             ],
