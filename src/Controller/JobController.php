d739463915178af92c7fbe224b3c3fbf7b711856
Jon Cram <webignition@gmail.com>
2026-06-10 14:32:11 +0100

    Return full job from status endpoint (#299)

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -6,41 +6,48 @@ use App\Entity\JobInterface;
 use App\EntityFactory\JobFactory as JobEntityFactory;
 use App\Exception\InvalidUserException;
 use App\ObjectFactory\JobFactoryInterface as JobModelFactory;
-use App\ObjectFactory\JobStateFactory;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[Route('/job/{label<[A-Z0-9]{26,32}>}', name: 'job_')]
-class JobController
+readonly class JobController
 {
+    public function __construct(
+        private JobModelFactory $jobModelFactory,
+    ) {}
+
     /**
      * @param non-empty-string $label
      */
     #[Route(name: 'create', methods: ['POST'])]
     public function create(
         JobEntityFactory $jobEntityFactory,
-        JobModelFactory $jobModelFactory,
         UserInterface $user,
         string $label
     ): Response {
         try {
-            $jobEntity = $jobEntityFactory->createForUserAndJob($user, $label);
-
-            return new JsonResponse($jobModelFactory->create($jobEntity));
+            return $this->createJobResponse(
+                $jobEntityFactory->createForUserAndJob($user, $label)
+            );
         } catch (InvalidUserException) {
             return new JsonResponse(null, 403);
         }
     }
 
-    #[Route(name: 'status', methods: ['GET'])]
-    public function status(JobStateFactory $jobStateFactory, UserInterface $user, ?JobInterface $job): Response
+    #[Route(name: 'get', methods: ['GET'])]
+    public function get(UserInterface $user, ?JobInterface $job): Response
     {
         if (null === $job || $job->getUserId() !== $user->getUserIdentifier()) {
             return new Response(null, 404);
         }
 
-        return new JsonResponse($jobStateFactory->create($job));
+        return $this->createJobResponse($job);
+    }
+
+    private function createJobResponse(JobInterface $job): Response
+    {
+        return new JsonResponse($this->jobModelFactory->create($job));
     }
 }
